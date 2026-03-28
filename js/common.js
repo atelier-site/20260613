ebd1e3ff2362faae83e770437bd5d278e927b1ca
20240421 <157558098+20240421@users.noreply.github.com>
2026-03-29 01:43:57 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(window).on('load',function(){
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  });
+
+$('.slider').slick({
+    arrows: false,
+    autoplay:true,
+    fade:true, 
+});
+
+$(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 100;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('on');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+  });
+
+  $(function() {
+    $("#arch1").arctext({
+        radius : 180,
+    });
+
+    $("#arch2").arctext({
+      radius : 180,
+    });
+  })
